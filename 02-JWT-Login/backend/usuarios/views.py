d09dde19dc646e5bff6075be41955de6fa91fd05
arnoldbgm <arnold.gallegosm@gmail.com>
2024-09-01 19:30:42 -0500

    Pasos para hacer cambio de table usuarios django

--- 02-JWT-Login/backend/usuarios/views.py
+++ 02-JWT-Login/backend/usuarios/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import UserSerializer
-
+from rest_framework import generics
 
 class UserRegister(APIView):
 # debes de especificar de que metodo es POST - GET - PUT - PATCH - DELETE
@@ -24,3 +24,18 @@ class UserRegister(APIView):
             "content": serializer.data
          }, status = status.HTTP_201_CREATED)
       return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+   
+
+
+class UserRegister(generics.CreateAPIView):
+   serializer = UserSerializer(data=request.data)
+
+   def create(self, request):
+      if self.serializer.is_valid():
+         self.serializer.save()
+         return Response({
+            "msg": "Registro exitoso 😁😁",
+            "content": self.serializer.data
+         }, status = status.HTTP_201_CREATED)
+      return Response(self.serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
