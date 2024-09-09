68e425f5da99a3f010b6f1cd46cba5dc9676b48d
arnoldbgm <arnold.gallegosm@gmail.com>
2024-09-08 22:08:15 -0500

    Repaso de Node

--- 05-NodePrisma/controllers/product.controller.js
+++ 05-NodePrisma/controllers/product.controller.js
@@ -10,3 +10,106 @@ export const getProduct = async (req, res) => {
     data: productsRes,
   });
 };
+
+export const getCategorias = async (req, res) => {
+  // await    prisma.TABLA.METODO
+  // El findMany devuelve todos los registros
+  const resCategoria = await prisma.categoria.findMany();
+  return res.json({
+    data: resCategoria,
+  });
+};
+
+// Crear una consulta que liste todos los productos de una categoria especifica
+export const getProductCat = async (req, res) => {
+  // api/v1/productoscat?catego=Ropa
+
+  // variable
+  // catego = Ropa
+  const { catego } = req.query;
+
+  try {
+    const resCategoria = await prisma.categoria.findFirst({
+      where: {
+        nombre: catego,
+      },
+      include: {
+        productos: true,
+      },
+    });
+    return res.json(resCategoria.productos);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+// Crear una consulta que ordene el precio de los productos de mayor a menor
+
+// api/v1/productosorden?order=asc
+export const getProductOrder = async (req, res) => {
+  let { order = "asc" } = req.query;
+
+  if (order !== "desc") {
+    order = "asc";
+  }
+
+  try {
+    const getProducts = await prisma.producto.findMany({
+      orderBy: {
+        // Aqui especifico que columna se va ordenar
+        precio: order,
+      },
+    });
+    res.json(getProducts);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+// Crear una consulta que me traiga los productos por su nombre
+export const getProductName = async (req, res) => {
+  // api/v1/productosnombre?nombre=mouse
+  // mouse  MOUSE  mOuSe
+
+  const { nombre } = req.query;
+
+  try {
+    const getProductoN = await prisma.producto.findMany({
+      where: {
+        nombre: {
+          contains: nombre,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    res.json(getProductoN);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+export const getProductPagination = async (req, res) => {
+  // api/v1/getproduct
+  const { page = 1, pageSize = 5 } = req.query;
+
+  const skip = (page - 1) * pageSize;
+
+  try {
+    const dataProduct = await prisma.producto.findMany({
+      // Aqui vamos a usar la paginacion
+      skip: parseInt(skip),
+      take: parseInt(pageSize), //Cuantos productos quiero que se meuestre por pagina
+      orderBy:{
+        id: "asc"
+      }
+    });
+    res.json(dataProduct);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
--- 05-NodePrisma/router/product.routes.js
+++ 05-NodePrisma/router/product.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 
-import { getProduct } from "../controllers/product.controller.js";
+import {
+  getCategorias,
+  getProduct,
+  getProductCat,
+  getProductName,
+  getProductOrder,
+  getProductPagination,
+} from "../controllers/product.controller.js";
 
 // Esta parte nos permite exportar nuestra funcion
 export const api = Router();
 
 // Aqui estamos declarando la url y asociandolo a un controlador
 api.get("/productos", getProduct);
+api.get("/categorias", getCategorias);
+api.get("/productoscat", getProductCat);
+api.get("/productoprice", getProductOrder);
+api.get("/productosnombre", getProductName);
+api.get("/allproduct", getProductPagination)
